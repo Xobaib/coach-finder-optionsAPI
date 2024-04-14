80f0698081c55b7b6ff8fbf9641eda3d4a8b2502
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-04-14 14:26:44 +0430

    Outputting Incoming Requests (Messages)

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,1 +1,20 @@
-export default {};
+export default {
+  requests(state) {
+    return state.requests;
+  },
+
+  hasRequests(state) {
+    // WAY 1:
+    // if (state.requests.length > 0) {
+    //   return true;
+    // } else {
+    //   return false;
+    // };
+
+    // WAY 2:
+    // return state.requests.length > 0 ? true : false;
+
+    // WAY 3:
+    return state.requests && state.requests.length > 0;
+  },
+};
