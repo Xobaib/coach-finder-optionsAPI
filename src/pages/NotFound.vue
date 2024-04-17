7e18f88dcf134779bdd88f2458068c57be7a6bf2
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-04-17 10:46:27 +0430

    The Not Found Page & Summary

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-  <div></div>
+  <section>
+    <BaseCard>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe checkout all our
+        <RouterLink to="/coaches">coaches</RouterLink>
+      </p>
+    </BaseCard>
+  </section>
 </template>
