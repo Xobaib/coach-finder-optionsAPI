0f1701ac0b8e54644e441f5de6cd5b3557e8a509
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-04-17 14:24:55 +0430

    Authentication: Adding an Authentication Page (Login & Signup)

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,156 @@
+<template>
+  <BaseCard>
+    <form @submit.prevent="submitForm">
+      <div class="form-control" :class="{ errors: !email.isValid }">
+        <label for="email">E-Mail</label>
+        <input
+          type="email"
+          id="email"
+          v-model.trim="email.value"
+          @blur="clearValidity('email')"
+        />
+        <p v-if="!email.isValid">Please enter your email address</p>
+      </div>
+      <div class="form-control" :class="{ errors: !password.isValid }">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          v-model.trim="password.value"
+          @blur="clearValidity('password')"
+        />
+        <p v-if="!password.isValid">
+          Your password must not be less than 6 characters
+        </p>
+      </div>
+      <p v-if="!formIsValid">Please fix the above errors and try again</p>
+      <BaseButton type="submit">{{ submitButtonCaption }}</BaseButton>
+      <BaseButton type="button" mode="flat" @click="switchAuthMode">{{
+        switchModeButtonCaption
+      }}</BaseButton>
+    </form>
+  </BaseCard>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: {
+        value: '',
+        isValid: true,
+      },
+
+      password: {
+        value: '',
+        isValid: true,
+      },
+
+      formIsValid: true,
+
+      mode: 'login',
+    };
+  },
+
+  methods: {
+    formValidation() {
+      this.formIsValid = true;
+
+      if (this.email.value === '' || !this.email.value.includes('@')) {
+        this.email.isValid = false;
+        this.formIsValid = false;
+      }
+      if (!this.password.value || this.password.value.length < 6) {
+        this.password.isValid = false;
+        this.formIsValid = false;
+      }
+    },
+
+    submitForm() {
+      this.formValidation();
+
+      if (!this.formIsValid) {
+        return;
+      }
+
+      console.log(this.email.value);
+      console.log(this.password.value);
+
+      this.email.value = '';
+      this.password.value = '';
+    },
+
+    clearValidity(input) {
+      this[input].isValid = true;
+    },
+
+    switchAuthMode() {
+      if (this.mode === 'login') {
+        this.mode = 'signup';
+      } else {
+        this.mode = 'login';
+      }
+    },
+  },
+
+  computed: {
+    submitButtonCaption() {
+      if (this.mode === 'login') {
+        return 'Login';
+      } else {
+        return 'Signup';
+      }
+    },
+
+    switchModeButtonCaption() {
+      if (this.mode === 'login') {
+        return 'Signup instead';
+      } else {
+        return 'Login instead';
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.errors input {
+  background-color: rgb(240, 195, 195);
+}
+</style>
