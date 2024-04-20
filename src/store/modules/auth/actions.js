303f4773ac4ace68b682c38c7f9f7ffdce5bceb3
Xobaib Mahmoodi <xobaib2001.m@gmail.com>
2024-04-20 15:34:14 +0430

    Adding Auto Login

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,48 +1,37 @@
 export default {
   async signup(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBBj_-eQuNZLUHO_AlEt1WvaBAyw2AoSfs',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(
-        responseData.message || 'Faild to authenticate. Check your login data'
-      );
-      throw error;
-    }
-
-    console.log(responseData);
-
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.loaclId,
-      tokeExpiration: responseData.expiresIn,
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
     });
   },
 
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBBj_-eQuNZLUHO_AlEt1WvaBAyw2AoSfs',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBBj_-eQuNZLUHO_AlEt1WvaBAyw2AoSfs';
+
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBBj_-eQuNZLUHO_AlEt1WvaBAyw2AoSfs';
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     const responseData = await response.json();
 
@@ -55,12 +44,28 @@ export default {
     }
 
     console.log(responseData);
+
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
 
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokeExpiration: responseData.expiresIn,
     });
+  },
+
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokeExpiration: null,
+      });
+    }
   },
 
   logout(context) {
